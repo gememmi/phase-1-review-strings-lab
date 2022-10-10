1ec9ce5d2eea6109f1dbbaa282db50dff1033866
Emily <emelizadaniels@gmail.com>
2022-10-10 14:42:47 -0400

    commit tree 07e5d70379adb47499dcc68fecfc7c68bb7777c4

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+let currentUser = 'Emily';
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+currentUser[0];
+'Emily'.charAt(0);
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
